4d8ba19236589d839a8a2f7a377a84d9c690fe90
htet oo wai yan <www.htetooaa@gmail.com>
2023-06-08 11:35:15 +0630

    welcome file

---
+++ src/main/java/tutorial/Hello.java
@@ -0,0 +1,10 @@
+package main.java.tutorial;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
